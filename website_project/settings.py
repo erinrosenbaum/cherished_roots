a31f2efe1944fad42db8a586fddd560eeb4f7b0f
Erin Rosenbaum <erinrosenbaum@yahoo.com>
2019-03-07 04:15:46 -0700

    background revisions

--- website_project/settings.py
+++ website_project/settings.py
@@ -125,6 +125,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'website_project/static/')
+]
+
+
+
+
+
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
