3281afe89b05c0c367646256804d8eafb3d6a5df
Erin Rosenbaum <erinrosenbaum@yahoo.com>
2019-03-08 05:50:31 -0700

    modified

--- website_project/settings.py
+++ website_project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -132,12 +132,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'website_project/static/'),
 ]
 
-
-
-
-
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
